77eecf66cd63ab06e56623197a1ec5d8c2116a26
Huzeyfe Yetkiner <huzeyfe.yetkiner@2ntech.com.tr>
2025-10-27 01:06:39 +0300

    Update LocationCard component to center-align location name text for improved readability.

--- components/location-card.tsx
+++ components/location-card.tsx
@@ -7,7 +7,7 @@ function LocationCard({ location }: { location: Location }) {
 			href={`/location/${location.id}`}
 			className="p-10 bg-[#FAFAFA] shadow-card-3 flex flex-col items-center justify-center cursor-pointer"
 		>
-			<h2 className="text-lg text-[#000000DE] font-medium">
+			<h2 className="text-lg text-[#000000DE] font-medium text-center">
 				{location.name}
 			</h2>
 			<p className="text-sm text-[#00000099] font-medium">
