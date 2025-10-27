354ef92e9a38f30c561ca73f04441a167be11e57
Huzeyfe Yetkiner <huzeyfe.yetkiner@2ntech.com.tr>
2025-10-27 20:29:05 +0300

    Refactor Header component to improve layout and navigation; add responsive links for Characters, Locations, and Episodes, and enhance mobile menu functionality with HamburgerIcon.

--- components/header.tsx
+++ components/header.tsx
@@ -19,18 +19,42 @@ function Header() {
 	return (
 		<>
 			<div className="w-full h-16 bg-white shadow-card-3 flex items-center justify-between px-6 relative z-60">
-				<Link href="/" className="">
-					<HeaderIcon />
-				</Link>
-				<div>
-					<button
-						onClick={() => setIsMenuOpen(isMenuOpen ? false : true)}
-						className="cursor-pointer"
-					>
-						<HamburgerIcon />
-					</button>
+				<div className="container mx-auto flex items-center justify-between gap-12">
+					<Link href="/" className="">
+						<HeaderIcon />
+					</Link>
+					<div className="hidden md:block">
+						<div className="flex items-center gap-12">
+							<Link href="/" className="text-2xl font-bold">
+								Characters
+							</Link>
+							<Link
+								href="/location"
+								className="text-2xl font-bold"
+							>
+								Locations
+							</Link>
+							<Link
+								href="/episode"
+								className="text-2xl font-bold"
+							>
+								Episodes
+							</Link>
+						</div>
+					</div>
+					<div className="md:hidden">
+						<button
+							onClick={() =>
+								setIsMenuOpen(isMenuOpen ? false : true)
+							}
+							className="cursor-pointer md:hidden"
+						>
+							<HamburgerIcon />
+						</button>
+					</div>
 				</div>
 			</div>
+
 			<Sheet open={isMenuOpen} onClose={() => setIsMenuOpen(false)}>
 				<div className="flex flex-col gap-2 items-center justify-center">
 					<button
